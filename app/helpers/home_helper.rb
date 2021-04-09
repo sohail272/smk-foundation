9831c13f7b3ff8bf0990ffc71c7022cdc4fd39d2
Abdul Sohail <abdulsohail@Abduls-MacBook-Pro.local>
2021-04-10 03:00:15 +0530

    added home page with bootstrap and jquery

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,12 @@
+module HomeHelper
+  def number_to_indian_currency(number)
+    if number
+      string = number.to_s.split('.')
+      number = string[0].gsub(/(\d+)(\d{3})$/){ p = $2;"#{$1.reverse.gsub(/(\d{2})/,'\1,').reverse},#{p}"}
+      number = number.gsub(/^,/, '') + '.' + string[1] if string[1]
+      # remove leading comma
+      number = number[1..-1] if number[0] == 44
+    end
+    "Rs.#{number}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root to:  'home#index'
+
+  devise_for :admins
   namespace :admin do
       resources :admins
       resources :donors
       resources :mode_of_payments
-
-      root to: "admins#index"
     end
-  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
